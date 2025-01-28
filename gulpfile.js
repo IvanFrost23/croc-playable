1e862eb6e67a4f152a0d2556b4774eb79ad1a775
Ivan Morozov <morozov.medo@gmail.com>
2025-01-28 13:50:36 +0500

    add gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,28 @@ const inline = require('gulp-inline');
 const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
+const base64 = require('gulp-base64');
+const path = require('path');
 
 // Задача: Минификация JS, инлайнинг и минификация HTML
 gulp.task('inline-and-minify', function () {
     // Минифицируем JS-файлы отдельно
-    gulp.src('src/**/*.js') // Все JS-файлы
+    gulp.src('src/js/**/*.js') // Все JS-файлы
         .pipe(uglify()) // Минификация JS
         .pipe(gulp.dest('dist/js')); // Сохраняем минифицированные файлы
 
-    // Далее обрабатываем HTML
+    // Далее обрабатываем HTML и инлайним все файлы
     return gulp.src('src/index.html') // Исходный HTML
+        .pipe(base64({
+            maxImageSize: 3000 * 1024, // Лимит на 10 KB (можно менять)
+            baseDir: path.join(__dirname, 'src'), // Папка с исходниками
+            debug: true // Включить вывод информации о процессе
+        })) // Инлайн изображений в Base64
         .pipe(
             inline({
                 base: 'src/', // Базовый путь для ресурсов (изображений, скриптов и т.д.)
-                ignore: [] // Не игнорируем ничего
+                ignore: [], // Не игнорируем ничего
+                css: true
             })
         )
         .pipe(

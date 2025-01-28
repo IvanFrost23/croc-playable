bafcbb408a483c248b2623587b9bee02ea025e0e
Ivan Morozov <morozov.medo@gmail.com>
2025-01-28 18:46:53 +0500

    split styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,53 @@ const gulp = require('gulp');
 const inline = require('gulp-inline');
 const htmlmin = require('gulp-htmlmin');
 const uglify = require('gulp-uglify');
-const rename = require('gulp-rename');
 const base64 = require('gulp-base64');
 const path = require('path');
+const cleanCSS = require('gulp-clean-css'); // Для минификации CSS
+const gulpif = require('gulp-if'); // Для условного применения
+
+// Функция для проверки, является ли файл CSS
+function isCSS(file) {
+    return file.extname === '.css';
+}
 
 gulp.task('inline-and-minify', function () {
+    // Минификация и копирование JS файлов
     gulp.src('src/js/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('docs/js'));
 
-    return gulp.src('src/index.html')
-        .pipe(base64({
-            maxImageSize: 3000 * 1024,
+    // Копирование картинок (если не нужно их преобразовывать в base64)
+    gulp.src('src/images/**/*')
+        .pipe(gulp.dest('docs/images'));
+
+    // Минификация и копирование CSS файлов с преобразованием картинок в base64
+    gulp.src('src/css/**/*.css')
+        .pipe(gulpif(isCSS, base64({
+            maxImageSize: 3000 * 1024,  // Максимальный размер изображения для base64
             baseDir: path.join(__dirname, 'src'),
             debug: true
+        })))
+        .pipe(cleanCSS())  // Минификация CSS
+        .pipe(gulp.dest('docs/css'));
+
+    // Обработка HTML: добавление стилей и скриптов inline, без минификации CSS в процессе
+    return gulp.src('src/index.html')
+        .pipe(inline({
+            base: 'src/',  // Указываем исходную папку для стилей и скриптов
+            ignore: []
         }))
-        .pipe(
-            inline({
-                base: 'src/',
-                ignore: [],
-                css: true
-            })
-        )
+        .pipe(base64({
+            maxImageSize: 3000 * 1024,  // Максимальный размер изображения для base64
+            baseDir: path.join(__dirname, 'src'),
+            debug: true
+        })) // Преобразуем изображения в base64 в HTML
         .pipe(
             htmlmin({
                 collapseWhitespace: true,
                 removeComments: true,
-                minifyJS: true,
-                minifyCSS: true
+                minifyCSS: true, // Минификация CSS
+                minifyJS: true
             })
         )
         .pipe(gulp.dest('docs/'));
